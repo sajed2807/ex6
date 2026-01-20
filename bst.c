2bcb71635d87c1c71eacd41322af20805c8e4ad1
sajed2807 <isasajed@outlook.co.il>
2026-01-21 00:00:21 +0200

    Update bst.c

--- bst.c
+++ bst.c
@@ -1,2 +1,34 @@
 #include <stdlib.h>
 #include "bst.h"
+/*
+Name: Sajed Isa
+ID: 325949089
+*/
+
+#ifndef BST_H
+#define BST_H
+
+typedef struct BSTNode {
+    void* data;
+    struct BSTNode* left;
+    struct BSTNode* right;
+} BSTNode;
+
+typedef struct {
+    BSTNode* root;
+    int (compare)(void, void*);
+    void (print)(void);
+    void (freeData)(void);
+} BST;
+
+typedef enum { PREORDER = 1, INORDER, POSTORDER } TraversalOrder;
+
+BST* bst_create(int (cmp)(void, void*),
+                void (print)(void),
+                void (freeData)(void));
+
+int bst_insert(BST* tree, void* data);
+void bst_traverse(BST* tree, TraversalOrder order);
+void bst_free(BST* tree);
+
+#endif
