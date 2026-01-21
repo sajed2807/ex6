cf80418a7bd33cf06a1a88e9537ed678f339e953
sajed2807 <isasajed@outlook.co.il>
2026-01-21 23:17:21 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,4 +1,17 @@
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "utils.h"
+
+int getInt(const char* prompt) {
+    int val;
+    printf("%s", prompt);
+    scanf("%d", &val);
+    return val;
+}
+
+int compareItems(void* a, void* b) { return 0; }
+void printItem(void* a) {}
+void freeItem(void* a) { free(a); }
+int compareMonsters(void* a, void* b) { return 0; }
+void printMonster(void* a) {}
+void freeMonster(void* a) { free(a); }
