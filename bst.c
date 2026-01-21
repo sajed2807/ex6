fb60c964dbd9bce33f9431313336dfb71f5cc13b
sajed2807 <isasajed@outlook.co.il>
2026-01-21 23:15:57 +0200

    Update bst.c

--- bst.c
+++ bst.c
@@ -1,2 +1,19 @@
-#include <stdlib.h>
 #include "bst.h"
+#include <stdlib.h>
+
+BST* createBST(CompareFunc comp, PrintFunc print, FreeFunc freeD) {
+    BST* bst = malloc(sizeof(BST));
+    bst->root = NULL;
+    bst->compare = comp;
+    bst->print = print;
+    bst->freeData = freeD;
+    return bst;
+}
+
+void bstFree(BSTNode* root, FreeFunc freeD) {
+    if (!root) return;
+    bstFree(root->left, freeD);
+    bstFree(root->right, freeD);
+    freeD(root->data);
+    free(root);
+}
