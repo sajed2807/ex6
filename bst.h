36275bd8903d49c2c921207b84820ebaea7f8832
sajed2807 <isasajed@outlook.co.il>
2026-01-21 21:57:02 +0200

    Update bst.h

--- bst.h
+++ bst.h
@@ -23,3 +23,35 @@ void bstPostorder(BSTNode* root, void (*print)(void*));
 void bstFree(BSTNode* root, void (*freeData)(void*));
 
 #endif
+/*
+Name: Sajed Isa
+ID: 325949089
+*/
+
+#ifndef BST_H
+#define BST_H
+
+typedef struct BSTNode {
+    void* data;
+    struct BSTNode* left;
+    struct BSTNode* right;
+} BSTNode;
+
+typedef struct {
+    BSTNode* root;
+    int (compare)(void, void*);
+    void (print)(void);
+    void (freeData)(void);
+} BST;
+
+typedef enum { PREORDER = 1, INORDER, POSTORDER } TraversalOrder;
+
+BST* bst_create(int (cmp)(void, void*),
+                void (print)(void),
+                void (freeData)(void));
+
+int bst_insert(BST* tree, void* data);
+void bst_traverse(BST* tree, TraversalOrder order);
+void bst_free(BST* tree);
+
+#endif
