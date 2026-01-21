77e9973b31b3277854af240ec1894a70527f9f9c
sajed2807 <isasajed@outlook.co.il>
2026-01-21 22:17:39 +0200

    Clean up utils.h and update header guards
    Removed unnecessary comments and restructured header guards.

--- utils.h
+++ utils.h
@@ -5,22 +5,3 @@ int getInt(const char* prompt);
 char* getString(const char* prompt);
 
 #endif
-/*
-Name: Sajed Isa
-ID: 325949089
-*/
-
-#ifndef UTILS_H
-#define UTILS_H
-
-char* read_line(void);
-
-int compare_items(void*, void*);
-void print_item(void*);
-void free_item(void*);
-
-int compare_monsters(void*, void*);
-void print_monster(void*);
-void free_monster(void*);
-
-#endif
