f458e13b29c048763cb420db3c475857b868c02d
sajed2807 <isasajed@outlook.co.il>
2026-01-22 19:18:39 +0200

    Update bst.c

--- bst.c
+++ bst.c
@@ -1,36 +1,43 @@
 #include "bst.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(void*)) {
+BST* createBST(int (*cmp)(void*, void*),
+               void (*print)(void*),
+               void (*freeData)(void*)) 
+{
     BST* bst = malloc(sizeof(BST));
     if (!bst) return NULL;
+
     bst->root = NULL;
     bst->compare = cmp;
     bst->print = print;
     bst->freeData = freeData;
+
     return bst;
 }
 
 BSTNode* bstInsert(BSTNode* root, void* data, int (*cmp)(void*, void*)) {
     if (!root) {
-        BSTNode* n = malloc(sizeof(BSTNode));
-        n->data = data;
-        n->left = n->right = NULL;
-        return n;
+        BSTNode* node = malloc(sizeof(BSTNode));
+        if (!node) return NULL;
+        node->data = data;
+        node->left = node->right = NULL;
+        return node;
     }
-    if (cmp(data, root->data) < 0)
-        root->left = bstInsert(root->left, data, cmp);
-    else
-        root->right = bstInsert(root->right, data, cmp);
+
+    int res = cmp(data, root->data);
+    if (res < 0) root->left = bstInsert(root->left, data, cmp);
+    else if (res > 0) root->right = bstInsert(root->right, data, cmp);
     return root;
 }
 
 void* bstFind(BSTNode* root, void* data, int (*cmp)(void*, void*)) {
     if (!root) return NULL;
-    int c = cmp(data, root->data);
-    if (c == 0) return root->data;
-    if (c < 0) return bstFind(root->left, data, cmp);
-    return bstFind(root->right, data, cmp);
+    int res = cmp(data, root->data);
+    if (res == 0) return root->data;
+    else if (res < 0) return bstFind(root->left, data, cmp);
+    else return bstFind(root->right, data, cmp);
 }
 
 void bstInorder(BSTNode* root, void (*print)(void*)) {
@@ -60,5 +67,10 @@ void bstFree(BSTNode* root, void (*freeData)(void*)) {
     bstFree(root->right, freeData);
     if (freeData && root->data) freeData(root->data);
     free(root);
+}
+
+void insertBST(BST* tree, void* data) {
+    if (!tree) return;
+    tree->root = bstInsert(tree->root, data, tree->compare);
 }
 
