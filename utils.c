db0babc94e1c9d2336943d379183eafbd9e8764e
sajed2807 <isasajed@outlook.co.il>
2026-01-22 15:25:46 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,17 +1,22 @@
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int getInt(const char* prompt) {
     int val;
-    printf("%s", prompt);
-    scanf("%d", &val);
+    if (prompt) printf("%s", prompt);
+    if (scanf("%d", &val) != 1) {
+        while (getchar() != '\n');
+        return -1;
+    }
     return val;
 }
 
-int compareItems(void* a, void* b) { return 0; }
-void printItem(void* a) {}
-void freeItem(void* a) { free(a); }
-int compareMonsters(void* a, void* b) { return 0; }
-void printMonster(void* a) {}
-void freeMonster(void* a) { free(a); }
+// Fixed: Using (void) to prevent unused parameter error
+int compareItems(void* a, void* b) { (void)a; (void)b; return 0; }
+void printItem(void* a) { (void)a; }
+void freeItem(void* a) { if (a) free(a); }
+int compareMonsters(void* a, void* b) { (void)a; (void)b; return 0; }
+void printMonster(void* a) { (void)a; }
+void freeMonster(void* a) { if (a) free(a); }
