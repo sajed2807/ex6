87cf7e0e1d62cda451be0fdaef6a9bc93dadfd24
sajed2807 <isasajed@outlook.co.il>
2026-01-22 21:27:00 +0200

    Update bst.c

--- bst.c
+++ bst.c
@@ -27,53 +27,42 @@ BST* createBST(int (cmp)(void, void*), void (print)(void), void (freeData)(void)
     tree->freeData = freeData;
     return tree;
 }
-
-static BSTNode* insertHelper(BSTNode* root, void* data, int (cmp)(void, void*)) {
+  
+BSTNode* bstInsert(BSTNode* root, void* data, int (cmp)(void, void*)) {
     if (root == NULL) return createNode(data);
     int res = cmp(data, root->data);
     if (res < 0)
-        root->left = insertHelper(root->left, data, cmp);
+        root->left = bstInsert(root->left, data, cmp);
     else if (res > 0)
-        root->right = insertHelper(root->right, data, cmp);
+        root->right = bstInsert(root->right, data, cmp);
     return root;
 }
 
-void insertBST(BST* tree, void* data) {
-    if (!tree) return;
-    tree->root = insertHelper(tree->root, data, tree->compare);
-}
-
-void bstPrintPreorder(BSTNode* root, void (print)(void)) {
+void bstPreorder(BSTNode* root, void (print)(void)) {
     if (!root) return;
     print(root->data);
-    bstPrintPreorder(root->left, print);
-    bstPrintPreorder(root->right, print);
+    bstPreorder(root->left, print);
+    bstPreorder(root->right, print);
 }
 
-void bstPrintInorder(BSTNode* root, void (print)(void)) {
+void bstInorder(BSTNode* root, void (print)(void)) {
     if (!root) return;
-    bstPrintInorder(root->left, print);
+    bstInorder(root->left, print);
     print(root->data);
-    bstPrintInorder(root->right, print);
+    bstInorder(root->right, print);
 }
 
-void bstPrintPostorder(BSTNode* root, void (print)(void)) {
+void bstPostorder(BSTNode* root, void (print)(void)) {
     if (!root) return;
-    bstPrintPostorder(root->left, print);
-    bstPrintPostorder(root->right, print);
+    bstPostorder(root->left, print);
+    bstPostorder(root->right, print);
     print(root->data);
 }
 
-static void destroyNodes(BSTNode* root, void (freeData)(void)) {
+void bstFree(BSTNode* root, void (freeData)(void)) {
     if (!root) return;
-    destroyNodes(root->left, freeData);
-    destroyNodes(root->right, freeData);
+    bstFree(root->left, freeData);
+    bstFree(root->right, freeData);
     if (freeData) freeData(root->data);
     free(root);
-}
-
-void destroyBST(BST* tree) {
-    if (!tree) return;
-    destroyNodes(tree->root, tree->freeData);
-    free(tree);
 }
