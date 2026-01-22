5b3fa24758a0ff45532ed73708aae08abc74dadd
sajed2807 <isasajed@outlook.co.il>
2026-01-22 21:10:37 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -10,91 +10,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include "utils.h"
-#include "game.h"
 
-char* readLine() {
+int getInt(const char* prompt) {
+    printf("%s", prompt);
+    int value;
+    scanf("%d", &value);
+    while (getchar() != '\n');
+    return value;
+}
+
+char* getString(const char* prompt) {
+    printf("%s", prompt);
     char buffer[1024];
+    fgets(buffer, sizeof(buffer), stdin);
     
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-        return NULL;
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len-1] == '\n') {
+        buffer[len-1] = '\0';
     }
     
-    size_t length = strlen(buffer);
-    if (length > 0 && buffer[length - 1] == '\n') {
-        buffer[length - 1] = '\0';
-    }
-    
-    char* line = (char*)malloc(strlen(buffer) + 1);
-    if (line == NULL) {
-        return NULL;
-    }
-    
-    strcpy(line, buffer);
-    return line;
-}
-
-Item* createItem(const char* name, ItemType type, int value) {
-    Item* newItem = (Item*)malloc(sizeof(Item));
-    if (newItem == NULL) {
-        return NULL;
-    }
-    
-    newItem->name = (char*)malloc(strlen(name) + 1);
-    if (newItem->name == NULL) {
-        free(newItem);
-        return NULL;
-    }
-    
-    strcpy(newItem->name, name);
-    newItem->type = type;
-    newItem->value = value;
-    
-    return newItem;
-}
-
-Monster* createMonster(const char* name, MonsterType type, int hp, int attack) {
-    Monster* newMonster = (Monster*)malloc(sizeof(Monster));
-    if (newMonster == NULL) {
-        return NULL;
-    }
-    
-    newMonster->name = (char*)malloc(strlen(name) + 1);
-    if (newMonster->name == NULL) {
-        free(newMonster);
-        return NULL;
-    }
-    
-    strcpy(newMonster->name, name);
-    newMonster->type = type;
-    newMonster->hp = hp;
-    newMonster->maxHp = hp;
-    newMonster->attack = attack;
-    
-    return newMonster;
-}
-
-Room* findRoomById(Room* head, int id) {
-    Room* current = head;
-    
-    while (current != NULL) {
-        if (current->id == id) {
-            return current;
-        }
-        current = current->next;
-    }
-    
-    return NULL;
-}
-
-Room* findRoomByCoordinates(Room* head, int x, int y) {
-    Room* current = head;
-    
-    while (current != NULL) {
-        if (current->x == x && current->y == y) {
-            return current;
-        }
-        current = current->next;
-    }
-    
-    return NULL;
+    char* str = (char*)malloc(strlen(buffer) + 1);
+    if (str) strcpy(str, buffer);
+    return str;
 }
